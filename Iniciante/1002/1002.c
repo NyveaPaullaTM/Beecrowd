b98ef8a6f14ebeaedced6f7782f7aae742322fa9
NyveaPaullaTM <nyveapaullatm@gmail.com>
2026-02-05 00:29:22 -0300

    Add files via upload

---
+++ Iniciante/1002/1002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const double PI = 3.14159;
+
+int main(){
+    double raio, area;
+    
+    scanf("%lf", &raio);
+    
+    area = raio * raio * PI;
+    
+    printf("A=%.4lf\n", area); 
+    
+    
+    return 0;
+}
