3e2b1e06d7b3fcbb7a24ba1dceb148a6262a0428
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-03-01 23:36:24 -0300

    Create 1066.c

---
+++ Iniciante/1066 - Pares, Ímpares, Positivos e Negativos/1066.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i, num, par, impar, pos, neg;
+    
+    par = impar = pos = neg = 0;
+    
+    for(i = 0; i < 5; i++){
+        scanf("%d", &num);
+        
+        if(num > 0){
+            pos++;
+        }
+        else if(num < 0) neg++;
+        
+        if(num%2 == 0){
+            par++;
+        }
+        else impar++;
+    }
+    
+    printf("%d valor(es) par(es)\n", par);
+    printf("%d valor(es) impar(es)\n", impar);
+    printf("%d valor(es) positivo(s)\n", pos);
+    printf("%d valor(es) negativo(s)\n", neg);
+    
+    return 0; 
+}
