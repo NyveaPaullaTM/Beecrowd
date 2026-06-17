2f539631b1e2afe90f3a166fb0732ea70d601231
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-06-17 09:58:38 -0300

    Create 1143.c

---
+++ Iniciante/1143 - Quadrado e ao Cubo/1143.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num;
+    
+    scanf("%d", &num);
+    
+    for(int i = 1; i <= num; i++){
+        printf("%d ", i);
+        printf("%d ", i*i);
+        printf("%d\n", i*i*i);
+    }
+    
+    return 0;
+}
