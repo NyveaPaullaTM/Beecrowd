2d7ffe03fd306e8d0855240904e0e09a6b7f1947
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-06-24 10:31:13 -0300

    Create 1173.c

---
+++ Iniciante/1173 - Preenchimento de Vetor I/1173.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    int v[10];
+    
+    scanf("%d", &n); 
+    
+    for(int i = 0; i < 10; i++){
+        v[i] = n;
+        n *= 2;
+    }
+    
+    for(int i = 0; i < 10; i++){
+        printf("N[%d] = %d\n", i, v[i]); 
+    }
+    
+    return 0;
+}
