79158e87bb7da053a34578f6f7591d693b7e5d7b
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-06-24 10:28:23 -0300

    Create 1172.c

---
+++ Iniciante/1172 - Substituição em Vetor I/1172.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int v[10];
+    
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &v[i]);
+        
+        if(v[i] <= 0) v[i] = 1; 
+    }
+    
+    for(int i = 0; i < 10; i++){
+        printf("X[%d] = %d\n", i, v[i]); 
+    }
+    
+    return 0;
+}
