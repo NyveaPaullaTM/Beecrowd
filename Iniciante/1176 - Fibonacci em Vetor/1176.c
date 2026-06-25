bffc13fb786d8db0578c2a0806e5ae9a3398a08b
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-06-25 11:33:32 -0300

    Create 1176.c

---
+++ Iniciante/1176 - Fibonacci em Vetor/1176.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int t, n;
+    unsigned long long f[61];
+    
+    f[0] = 0;
+    f[1] = 1;
+    
+    for(int i = 2; i <= 60; i++){
+        f[i] = f[i-1] + f[i-2]; 
+    }
+    
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        printf("Fib(%d) = %llu\n", n, f[n]); 
+    }
+    
+    return 0;
+}
