ef38c214b38320d26cc32d7bdbac0da438ff7629
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-07-14 14:18:12 -0300

    Create 1589.c

---
+++ Iniciante/1589 - Bob Conduite/1589.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int t, r1, r2;
+    
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d %d", &r1, &r2);
+        
+        printf("%d\n", r1+r2); 
+    }
+    
+    return 0;
+}
