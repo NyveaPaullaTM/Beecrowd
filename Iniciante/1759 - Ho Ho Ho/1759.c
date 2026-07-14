0faa1263a7dfb557ee2e47c54ccd0f1dfb65f0d2
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-07-14 14:20:49 -0300

    Create 1759.c

---
+++ Iniciante/1759 - Ho Ho Ho/1759.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    
+    scanf("%d", &n); 
+    n--; 
+    
+    while(n--){
+        printf("Ho ");
+    }
+    
+    printf("Ho!\n"); 
+    
+    return 0;
+}
