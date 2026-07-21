43fcde680b42611f6793e87a0dff33cf449509a0
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-07-21 17:03:22 -0300

    Create 2028.c

---
+++ Iniciante/2028 - Sequência de Sequência/2028.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int n, caso;
+    caso = 1; 
+    
+    while(scanf("%d", &n) != EOF){
+        int numeros = 1;
+        
+        for(int i = 1; i <= n; i++){
+            numeros += i;
+        }
+        
+        if(numeros > 1){
+            printf("Caso %d: %d numeros\n", caso, numeros);
+        }
+        else{
+            printf("Caso %d: %d numero\n", caso, numeros);
+        }
+        
+        printf("0 ");
+        
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                printf("%d ", i);
+            }
+        }
+        printf("\n");
+        
+        caso++; 
+    }
+    
+    return 0;
+}
