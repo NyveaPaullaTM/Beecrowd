3ed7e266b5563ebd81cec3b8f13c25424ffade36
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-07-30 09:04:19 -0300

    Create 2152.c

---
+++ Iniciante/2152 - Pepe, Já Tirei a Vela!/2152.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int n, h, m, o;
+    
+    scanf("%d", &n);
+    
+    while(n--){
+        scanf("%d %d %d", &h, &m, &o);
+        
+        if(h < 10) printf("0%d:", h);
+        else printf("%d:", h);
+        
+        if(m < 10) printf("0%d - ", m);
+        else printf("%d - ", m); 
+        
+        if(o) printf("A porta abriu!\n");
+        else printf("A porta fechou!\n"); 
+    }
+    
+    return 0; 
+}
