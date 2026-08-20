10cb89768c06f282a1f2ffc79f68eb777d93582a
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-08-20 17:05:37 -0300

    Create 2172.c

---
+++ Iniciante/2172 - Evento/2172.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    unsigned long long x, m;
+    
+    while(1){
+        scanf("%llu %llu", &x, &m);
+        
+        if((x == 0)&&(m == 0)){
+            break;
+        }
+        
+        printf("%llu\n", x*m);
+    }
+    
+    return 0;
+}
