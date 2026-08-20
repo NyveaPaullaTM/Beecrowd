40c458ade5dcff32f2c0e98b7b19c5de91f1073b
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-08-20 17:22:18 -0300

    Create 2234.c

---
+++ Iniciante/2234 - Cachorros-Quentes/2234.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    double result;
+    
+    scanf("%d %d", &a, &b);
+    
+    result = (double)a/b;
+    
+    printf("%.2lf\n", result); 
+    
+    return 0;
+}
