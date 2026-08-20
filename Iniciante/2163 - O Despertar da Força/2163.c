d22840d0e9cc8a460f734fa99ecd2a3f33b1b544
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-08-20 09:51:43 -0300

    Create 2163.c

---
+++ Iniciante/2163 - O Despertar da Força/2163.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int n, m;
+    
+    if (scanf("%d %d", &n, &m) != 2) return 0;
+    
+    int matriz[n][m]; 
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%d", &matriz[i][j]); 
+        }
+    }
+    
+    for(int i = 1; i < n - 1; i++){
+        for(int j = 1; j < m - 1; j++){
+            
+            if(matriz[i][j] == 42){
+                if((matriz[i-1][j-1] == 7)&&(matriz[i-1][j] == 7)&&(matriz[i-1][j+1] == 7)&&(matriz[i][j-1] == 7)
+                &&(matriz[i][j+1] == 7)&&(matriz[i+1][j-1] == 7)&&(matriz[i+1][j] == 7)&&(matriz[i+1][j+1] == 7)){
+
+                    printf("%d %d\n", i+1, j+1); 
+                    return 0; 
+                }
+            }
+        }
+    }
+
+    printf("0 0\n"); 
+    
+    return 0;
+}
