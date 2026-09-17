904e9872eb043fa3dda5ae10ffa378e2078c1fb8
Nyvea Paulla <nyveapaullatm@gmail.com>
2026-09-17 19:33:38 -0300

    Create 2510.c

---
+++ Iniciante/2510 - Batmain/2510.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    scanf("%d", &n);
+
+    while(n--){
+        char nome[27];
+
+        scanf("%s", nome);
+
+        printf("Y\n"); 
+    }
+
+    return 0; 
+}
